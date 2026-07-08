9b4d670955ef4c5fd33c62b40abe4325fd6717de
roushan-jha <jharoushan20@gmail.com>
2026-07-08 13:19:16 +0530

    Implemented voting functionality api

--- backend/src/main/java/com/voting/voteSys/controller/PollController.java
+++ backend/src/main/java/com/voting/voteSys/controller/PollController.java
@@ -1,6 +1,7 @@
 package com.voting.voteSys.controller;
 
 import com.voting.voteSys.model.Poll;
+import com.voting.voteSys.request.Vote;
 import com.voting.voteSys.service.PollService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -32,5 +33,10 @@ public class PollController {
         return pollService.getPollById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/vote")
+    public void vote(@RequestBody Vote vote) {
+        pollService.vote(vote.getPollId(), vote.getOptionIndex());
     }
 }
--- backend/src/main/java/com/voting/voteSys/service/PollService.java
+++ backend/src/main/java/com/voting/voteSys/service/PollService.java
@@ -1,6 +1,7 @@
 package com.voting.voteSys.service;
 
 import com.voting.voteSys.Repository.PollRepository;
+import com.voting.voteSys.model.OptionVote;
 import com.voting.voteSys.model.Poll;
 import org.apache.logging.log4j.util.Lazy;
 import org.springframework.stereotype.Service;
@@ -27,5 +28,29 @@ public class PollService {
 
     public Optional<Poll> getPollById(Long id) {
         return pollRepository.findById(id);
+    }
+
+    public void vote(Long pollId, int optionIndex) {
+
+        // Get Poll from DB
+        Poll poll = pollRepository.findById(pollId)
+                .orElseThrow(() -> new RuntimeException("Poll not found"));
+
+        // Get Options
+        List<OptionVote> options = poll.getOptions();
+
+        // Validate Index
+        if(optionIndex < 0 || optionIndex >= options.size()) {
+            throw new IllegalArgumentException("Invalid option index");
+        }
+
+        // Get Selected Option
+        OptionVote selectedOption = options.get(optionIndex);
+
+        // Increment vote for selected option
+        selectedOption.setVoteCount(selectedOption.getVoteCount() + 1);
+
+        // save to DB
+        pollRepository.save(poll);
     }
 }
